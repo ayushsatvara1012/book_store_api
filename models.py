3906e2788a1c7344433b09cf0b67dfd5a6573602
Ayush Satvara <ayushsatvara@MacBookPro.lan>
2026-01-29 22:53:46 -0500

    structured the backend with files:models,database,exception,books/router

---
+++ models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    year: int
+
+class BookUpdate(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    year: Optional[int] = None
+
+class BookResponse(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: int
+
+class DeleteResponse(BaseModel):
+    message: str
+    book: Optional[BookResponse] = None
